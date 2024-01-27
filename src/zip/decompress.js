48e903530c6d6dfefab96581246891be364d1407
Mikhail Sharakou <msharakov@gmail.com>
2024-01-27 18:02:53 +0300

    add zlib

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream/promises";
+import { createGunzip } from "zlib";
+
+const sourceFile = join(dirname(fileURLToPath(import.meta.url)), "files", "archive.gz");
+const destinationFile = join(dirname(fileURLToPath(import.meta.url)), "files", "fileToCompress.txt");
+
 const decompress = async () => {
-    // Write your code here 
+  const readable = createReadStream(sourceFile);
+  const writable = createWriteStream(destinationFile);
+  const gunzipStream = createGunzip();
+
+  try {
+    await pipeline(readable, gunzipStream, writable);
+  } catch (error) {
+    throw error;
+  }
 };
 
 await decompress();
